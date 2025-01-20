4ef415c247475bb813edb6eabbf0236f4bd4e51b
Matsadura <zidanditoo1@gmail.com>
2025-01-20 17:42:01 +0100

    [ADD] File descriptor functions

---
+++ fd_func.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fd_func.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zzaoui <zzaoui@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/20 17:39:12 by zzaoui            #+#    #+#             */
+/*   Updated: 2025/01/20 17:41:09 by zzaoui           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+///**
+// * read_file - Open a file for reading
+// * @file_name: File name to open
+// * Return: The file descriptor
+// */
+//int read_file(char *file_name)
+//{
+//    int fd;
+//
+//    fd = open(file_name, O_RDONLY);
+//    return (fd);
+//}
+//
+///**
+// * write_file - Open a file for writing
+// * @file_name: File name to open
+// * Return: The file descriptor
+// */
+//int write_file(char *file_name)
+//{
+//    int fd;
+//
+//    fd = open(file_name, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+//    return (fd);
+//}
+
+/**
+ *
+ */
+int	open_file(char **file_name, char **env, char **mode)
+{
+	int	fd;
+
+	fd = -1;
+	if (mode == STDIN_FILENO)
+		fd = open(file_name, O_RDONLY);
+	else if (mode == STDOUT_FILENO)
+		fd = open(file_name, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if (fd == -1)
+	{
+		perror(file_name);
+		free_2darray(env);
+		exit(13);
+	}
+	return (fd);
+}
+
+/**
+ * dup_fd - Duplicates file descriptors
+ * @file_fd: open file descriptor
+ * @pipe_fd: pipe line file descriptors
+ */
+void	dup_fd(int pipe_fd[], int file_fd, int mode)
+{
+	if (mode == 0)
+	{
+		dup2(file_fd, 0);
+		close(file_fd);
+		dup2(pipe_fd[1], 1);
+		close(pipe_fd[1]);
+	}
+	else if (mode == 1)
+	{
+		dup2(file_fd, 1);
+		close(file_fd);
+		dup2(pipe_fd[0], 0);
+		close(pipe_fd[0]);
+	}
+}
+
+/**
+ * close_pipe - Closes the pipe file descriptors
+ * @pipe_fd: The pipe's file descriptors
+ */
+void	close_pipe(int pipe_fd[])
+{
+	close(pipe_fd[0]);
+	close(pipe_fd[1]);
+}
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: zzaoui <zzaoui@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 12:39:28 by zzaoui            #+#    #+#             */
-/*   Updated: 2025/01/13 16:22:54 by zzaoui           ###   ########.fr       */
+/*   Updated: 2025/01/20 17:37:09 by zzaoui           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 # include "libft/libft.h"
 # include <sys/wait.h>
+# include <errno.h>
+# include <string.h>
 
 /** 2D Array functions **/
 void	print_2darray(char **arr);
@@ -24,5 +26,10 @@ void	free_2darray(char **arr);
 char	**parse_env(char *envp[]);
 char	*concat_path(char *cmd, char *path);
 char	*get_cmd(char *cmd, char **path);
+
+/** File Descriptor functions**/
+void	dup_fd(int pipe_fd[], int file_fd, int mode);
+void	close_pipe(int pipe_fd[]);
+int		open_file(char **file_name, char **env, char **mode);
 
 #endif /* PIPEX_H */
