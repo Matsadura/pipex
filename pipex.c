1782167ea913af3eefffc20e020092676b700c54
Matsadura <zidanditoo1@gmail.com>
2025-01-21 18:16:51 +0100

    [FIX] pipex.c norminette

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: zzaoui <zzaoui@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 17:21:38 by zzaoui            #+#    #+#             */
-/*   Updated: 2025/01/21 17:24:29 by zzaoui           ###   ########.fr       */
+/*   Updated: 2025/01/21 18:16:39 by zzaoui           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,26 @@
 
 int	main(int ac, char **av, char *envp[])
 {
-	char	**env;
+	t_pipex	strct;
 	pid_t	id1;
 	pid_t	id2;
-	int		pipe_fd[2];
 
 	(void)ac;
-	env = parse_env(envp);
-	if (pipe(pipe_fd) == -1)
+	t_init(&strct, envp, av);
+	if (pipe(strct.pipe_fd) == -1)
 		exit(-1);
 	id1 = fork();
 	if (id1 == -1)
 		exit(-1);
 	if (id1 == 0)
-		handle_child(env, av, pipe_fd, STDIN_FILENO);
+		handle_child(strct, STDIN_FILENO, 1);
 	id2 = fork();
 	if (id2 == -1)
 		exit(-1);
 	if (id2 == 0)
-		handle_child(env, av, pipe_fd, STDOUT_FILENO);
-	free_2darray(env);
-	close_pipe(pipe_fd);
+		handle_child(strct, STDOUT_FILENO, 4);
+	free_2darray(strct.env);
+	close_pipe(strct.pipe_fd);
 	waitpid(id1, NULL, 0);
 	waitpid(id2, NULL, 0);
 	print_error();
