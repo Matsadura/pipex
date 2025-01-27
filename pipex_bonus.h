4bddfa75d43d116994575cd9dbb0b8f730d9b3d5
Matsadura <zidanditoo1@gmail.com>
2025-01-27 20:37:45 +0100

    [UPDATE] Add latest prototypes and Macros

--- pipex_bonus.h
+++ pipex_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   pipex_bonus.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zzaoui <zzaoui@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 12:39:28 by zzaoui            #+#    #+#             */
-/*   Updated: 2025/01/23 16:25:36 by zzaoui           ###   ########.fr       */
+/*   Updated: 2025/01/27 20:22:16 by zzaoui           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#ifndef PIPEX_BONUS_H
+# define PIPEX_BONUS_H
 
 # include "libft/libft.h"
 # include <sys/wait.h>
@@ -22,16 +22,34 @@
 # define CMD_404 ": command not found\n"
 # define CMD_403 ": permission denied\n"
 
+# define TRUE 1
+# define FALSE 0
+
+# define READ_END 0
+# define WRITE_END 1
+# define STDIN 0
+# define STDOUT 1
+# define STDERR 2
+
 /** Components structure **/
 typedef struct s_pipex
 {
 	char	**env;
 	char	**av;
+	char	*delim;
+	char	*infile;
+	char	*outfile;
+	int		out_perms;
+	int		ncmd;
 	int		pipe_fd[2];
 	int		status;
+	int		old_pipe;
+	int		in_fd;
+	int		out_fd;
+	int		is_hdoc;
 }	t_pipex;
 
-void	t_init(t_pipex *strct, char **envp, char **av);
+void	t_init(t_pipex *strct, char **envp, int ac, char **av);
 
 /** 2D Array functions **/
 
@@ -46,20 +64,30 @@ char	*get_cmd(char *cmd, char **path);
 
 /** File Descriptor functions**/
 
-void	dup_fd(t_pipex strct, char *file_name, int mode);
-void	close_pipe(int pipe_fd[]);
-int		open_file(char *file_name, t_pipex strct, char mode);
+void	dup_and_close(int fd1, int fd2, int mode);
+void	dup_and_close_file(int fd, int mode);
+int		open_input_file(t_pipex strct);
+int		open_output_file(t_pipex strct);
 
 /** Error handling functions **/
 
 void	write_error(int mode, char *error_msg);
 void	handle_error_components(char *file_name);
 void	print_error(int n);
-void	cleanup(t_pipex *strct, pid_t id1, pid_t id2);
+void	cleanup(t_pipex *strct, pid_t pid);
+void	check_pipe(int pipe_return, t_pipex strct);
 int		check_access(char *name, int mode);
+pid_t	fork_and_check(t_pipex strct);
 
 /** Execution Functions */
 
-void	handle_child(t_pipex strct, int mode, int index);
+void	execute_cmd(char *arg, char **env, int index);
+void	handle_child(t_pipex strct, int idx);
+void	handle_parent(t_pipex *strct, int idx);
 
-#endif /* PIPEX_H */
+/* Here document Functions */
+void	handle_heredoc(t_pipex *strct);
+void	put_and_print(char *line, int fd);
+void	write_in_heredoc(t_pipex strct);
+
+#endif /* PIPEX_BONUS_H */
